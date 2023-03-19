d1b67267b335fcaa03952e15a27479f495eadffa
malighters <chernenko.yevheniy@gmail.com>
2023-03-19 02:45:03 +0200

    adding docker and docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'user',
+      password: 'password',
+      database: 'db',
+      models: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
